8409ad06c51b6621042a8337c3465eae7671886a
kANNISTER <77148257+kANNISTER@users.noreply.github.com>
2024-11-14 19:29:56 -0700

    resolved error : middleware error

--- routes/test.js
+++ routes/test.js
@@ -1,29 +1,24 @@
-// Import required modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const bodyParser = require('body-parser');  
-// Import Patient and Physician models
-const Patient = require('./models/patient'); // Adjust path as necessary
-const Physician = require('./models/physician'); // Adjust path as necessary
+const bodyParser = require('body-parser');     // Parses JSON in body
+// // for mongDB
+
+// var indexRouter = require('./routes/index');
+var patientController = require('./controller/PatientController');
+
+// var studentsRouter = require('./routes/students');
+// var customersRouter = require('./routes/customers');
 
 
-
-// Import controller
-const patientController = require('./routes/PatientController');
-// Initialize the express app
-const app = express();
-// Middleware to parse JSON bodies
-app.use(express.json());
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-
 
 // This is to enable cross-origin access
 app.use(function (req, res, next) {
@@ -49,7 +44,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/students', studentsRouter);
 app.use('/patient', patientController);
 
 // catch 404 and forward to error handler
@@ -68,4 +65,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000);
 module.exports = app;
