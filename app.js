2c377a6e40c80c9e6956bc7f1362df9fb734ca91
kANNISTER <77148257+kANNISTER@users.noreply.github.com>
2024-12-13 17:55:19 -0700

    added cors import

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const cors = require('cors'); 
 var logger = require('morgan');
 
 const bodyParser = require('body-parser');  
